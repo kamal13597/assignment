c50f9ac05a1e564f7ba1074e22b243a363a5948f
kamal13597 <31628187+kamal13597@users.noreply.github.com>
2020-01-19 23:19:22 +0530

    Add files via upload

--- User.rb
+++ User.rb
@@ -1,21 +1,20 @@
 class User
 
-# Considering status field may have any of those values i.e active, inactive, pending and so on.
+	def initialize()
+		# @user =  		
+		@user = User.first  //considering this as the User Model file and has the status column in the database.
+	end
 
-def active?
-	status.equal? 'active'
-end
+	def active?
+		@user.status.equal? 'active'
+	end
 
-def pending?
-	status.equal? 'pending'
-end
+	def pending?
+		@user.status.equal? 'pending'
+	end
 
-def inactive?
-	status.equal? 'inactive'
-end	
-
-def so_on?
-	status.equal? 'so on'
-end
+	def inactive?
+		@user.status.equal? 'inactive'
+	end	
 
 end
